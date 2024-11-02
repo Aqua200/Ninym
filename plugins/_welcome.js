2e5710500420bf044b0042db4b50d49421cc8aaf
Aqua200 <chinquepapa@gmail.com>
2024-11-01 20:21:05 -0700

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,4 +1,5 @@
 import {WAMessageStubType} from '@whiskeysockets/baileys'
+import {WAMessageStubType} from '@whiskeysockets/baileys'
 import fetch from 'node-fetch'
 
 export async function before(m, {conn, participants, groupMetadata}) {
@@ -7,16 +8,70 @@ export async function before(m, {conn, participants, groupMetadata}) {
   let chat = global.db.data.chats[m.chat]
 
   if (chat.welcome && m.messageStubType == 27) {
-    let welcome = ` 《🌸》 𝙽𝚒𝚗𝚢𝚖🌸 \n—🍃̶᪶⃕⊹ ‧₊ 《•𝙱𝚒𝚎𝚗𝚟𝚎𝚗𝚒𝚍𝚘/𝚊•》🍃\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n—🍃̶᪶⃕⊹ ‧₊ 《•𝙱𝚒𝚎𝚗𝚟𝚎𝚗𝚒𝚍𝚘/𝚊•》🍃\n「 ${groupMetadata.subject} 」\n\n  𝐍𝐞𝐲𝐤𝐨𝐨𝐫🤍
-await conn.sendLuffy(m.chat, packname, textbot, welcome, img, img, redes, fkontak)
+    let wel = `┌─★ 𝙽𝚒𝚗𝚢𝚖🌸 \n│「 𝗕𝗶𝗲𝗻𝘃𝗲𝗻𝗶𝗱𝗼 ☁ 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🌺  𝗕𝗶𝗲𝗻𝘃𝗲𝗻𝗶𝗱𝗼/𝗮\n   │🌺  ${groupMetadata.subject}\n   └───────────────┈ ⳹`
+await conn.sendMini(m.chat, packname, dev, wel, img, img, channel, fkontak)
   }
 
   if (chat.welcome && m.messageStubType == 28) {
-    let bye = ` 《🌸》 𝙽𝚒𝚗𝚢𝚖🌸 \n🍃̶᪶⃕⊹ ‧₊ 《•𝙰𝚍𝚒𝚘𝚜•》🍃\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n🍃̶᪶⃕⊹ ‧₊ 《•𝚂𝚎 𝚏𝚞𝚎•》🍃\n🍃̶᪶⃕⊹ ‧₊ 《•𝚅𝚞𝚎𝚕𝚟𝚊 𝚙𝚛𝚘𝚗𝚝𝚘•》🍃\n\n 𝐍𝐞𝐲𝐤𝐨𝐨𝐫🤍`
-await conn.sendLuffy(m.chat, packname, textbot, bye, img, img, redes, fkontak)
+   let bye = `┌─★ 𝙽𝚒𝚗𝚢𝚖🌸 \n│「 𝗔𝗗𝗜𝗢𝗦 🌸 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🌺  𝗦𝗲 𝗳𝘂𝗲\n   │🌺 𝗡𝘂𝗻𝗰𝗮 𝘁𝗲 𝗾𝘂𝗶𝘀𝗶𝗺𝗼𝘀 𝗮𝗾𝘂𝗶\n   └───────────────┈ ⳹`
+await conn.sendMini(m.chat, packname, dev, bye, img2, img2, channel, fkontak)
   }
 
   if (chat.welcome && m.messageStubType == 32) {
-    let kick = ` 《🌸》 𝙽𝚒𝚗𝚢𝚖🌸 \n🍃̶᪶⃕⊹ ‧₊ 《•𝙰𝚍𝚒𝚘𝚜•》🍃\n「 @${m.messageStubParameters[0].split`@`[0]} 」\n🍃̶᪶⃕⊹ ‧₊ 《•𝚂𝚎 𝚏𝚞𝚎•》🍃\n🍃̶᪶⃕⊹ ‧₊ 《•𝙰𝚍𝚒𝚘𝚜 𝚊𝚙𝚝𝚘/𝚊 𝚙𝚊𝚛𝚊 𝚎𝚜𝚝𝚊𝚛 𝚎𝚗 𝚎𝚜𝚝𝚎 𝚐𝚛𝚞𝚙𝚘•》🍃\n\n 𝐍𝐞𝐲𝐤𝐨𝐨𝐫🤍`
-await conn.sendLuffy(m.chat, packname, textbot, kick, img, img, redes, fkontak)
+    let kick = `┌─★ 𝙽𝚒𝚗𝚢𝚖🌸 \n│「 𝗔𝗗𝗜𝗢𝗦 🌸 」\n└┬★ 「 @${m.messageStubParameters[0].split`@`[0]} 」\n   │🌺  𝗦𝗲 𝗳𝘂𝗲\n   │🌺 𝗡𝘂𝗻𝗰𝗮 𝘁𝗲 𝗾𝘂𝗶𝘀𝗶𝗺𝗼𝘀 𝗮𝗾𝘂𝗶\n   └───────────────┈ ⳹`
+await conn.sendMini(m.chat, packname, dev, kick, img2, img2, channel, fkontak)
 }}
+
+/*import { WAMessageStubType } from '@whiskeysockets/baileys';
+import fetch from 'node-fetch';
+
+export async function before(m, { conn, participants, groupMetadata }) {
+  if (!m.messageStubType || !m.isGroup) return true;
+
+  let img = imagen1
+  let welc = welcome;
+  let adi = adios;
+  let chat = global.db.data.chats[m.chat];
+  const getMentionedJid = () => {
+    return m.messageStubParameters.map(param => `${param}@s.whatsapp.net`);
+  };
+
+  let who = m.messageStubParameters[0] + '@s.whatsapp.net';
+  let user = global.db.data.users[who];
+
+  let userName = user ? user.name : await conn.getName(who);
+
+  if (chat.welcome && m.messageStubType === 27) {
+    this.sendMessage(m.chat, {
+      audio: { url: vn },
+      contextInfo: {
+        mentionedJid: getMentionedJid(),
+        "externalAdReply": {
+          "thumbnail": welc,
+          "title": "  ͟͞ Ｗ Ｅ Ｌ Ｃ Ｏ Ｍ Ｅ ͟͞  ",
+          "body": `${userName}!`,
+          "previewType": "PHOTO",
+          "thumbnailUrl": null,
+          "showAdAttribution": true,
+          sourceUrl: [yt, md, channel].sort(() => 0.5 - Math.random())[0]
+  }
+
+  if (chat.welcome && (m.messageStubType === 28 || m.messageStubType === 32)) {
+    this.sendMessage(m.chat, {
+      audio: { url: vn2 },
+      contextInfo: {
+        mentionedJid: getMentionedJid(),
+        "externalAdReply": {
+          "showAdAttribution": true,
+          "containsAutoReply": true,
+          "title": '  ͟͞ Ａ Ｄ Ｉ Ｏ́ Ｓ ͟͞  ',
+          body: `${userName}, se despide.`,
+          "previewType": "PHOTO",
+          "thumbnailUrl": '',
+          "thumbnail": adi,
+          "sourceUrl": redes
+        }
+      }
+    }, { quoted: fkontak });
+  }
+}*/
